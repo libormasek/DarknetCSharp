f6972153d62b7f98c4140a5a2076ba654cd79742
Libor Masek <libormasek@hotmail.com>
2025-10-23 19:10:17 +0800

    Changed DlIImport to work on both Linux and Windows

--- DarknetCSharp/Api/DarknetApi.cs
+++ DarknetCSharp/Api/DarknetApi.cs
@@ -1,36 +1,56 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace DarknetCSharp.Api;
 
 public static class DarknetApi
 {
-    private const string YoloLibraryName = "darknet.dll";
+    static DarknetApi()
+    {
+        NativeLibrary.SetDllImportResolver(typeof(DarknetApi).Assembly, ImportResolver);
+    }
 
-    [DllImport(YoloLibraryName, EntryPoint = "darknet_load_neural_network")]
+    private static IntPtr ImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+    {
+        if (libraryName == DarknetLibraryName)
+        {
+            string libPath = 
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? $"{DarknetLibraryName}.dll" :
+                RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? $"lib{DarknetLibraryName}.so" :
+                    throw new PlatformNotSupportedException();
+
+            return NativeLibrary.Load(libPath, assembly, searchPath);
+        }
+        return IntPtr.Zero;
+    }
+
+    private const string DarknetLibraryName = "darknet";
+
+    [DllImport(DarknetLibraryName, EntryPoint = "darknet_load_neural_network")]
     public static extern IntPtr LoadNeuralNetwork(string configurationFilename, string namesFilename, string weightsFilename);
 
-    [DllImport(YoloLibraryName, EntryPoint = "darknet_set_gpu_index")]
+    [DllImport(DarknetLibraryName, EntryPoint = "darknet_set_gpu_index")]
     public static extern void SetGpuIndex(int gpuIndex);
 
-    [DllImport(YoloLibraryName, EntryPoint = "darknet_show_version_info")]
+    [DllImport(DarknetLibraryName, EntryPoint = "darknet_show_version_info")]
     public static extern void ShowVersionInfo();
 
-    [DllImport(YoloLibraryName, EntryPoint = "darknet_network_dimensions")]
+    [DllImport(DarknetLibraryName, EntryPoint = "darknet_network_dimensions")]
     public static extern void NetworkDimensions(IntPtr networkPtr, out int w, out int h, out int c);
 
-    [DllImport(YoloLibraryName, EntryPoint = "network_predict_ptr")]
+    [DllImport(DarknetLibraryName, EntryPoint = "network_predict_ptr")]
     public static extern IntPtr NetworkPredictPtr(IntPtr networkPtr, float[] input);
 
-    [DllImport(YoloLibraryName, EntryPoint = "darknet_free_neural_network")]
+    [DllImport(DarknetLibraryName, EntryPoint = "darknet_free_neural_network")]
     public static extern void FreeNeuralNetwork(ref IntPtr networkPtr);
 
-    [DllImport(YoloLibraryName, EntryPoint = "do_nms_sort")]
+    [DllImport(DarknetLibraryName, EntryPoint = "do_nms_sort")]
     public static extern void DoNmsSort(IntPtr detections, int total, int classes, float threshold);
 
-    [DllImport(YoloLibraryName, EntryPoint = "free_detections")]
+    [DllImport(DarknetLibraryName, EntryPoint = "free_detections")]
     public static extern void FreeDetections(IntPtr detections, int count);
 
-    [DllImport(YoloLibraryName, EntryPoint = "get_network_boxes")]
+    [DllImport(DarknetLibraryName, EntryPoint = "get_network_boxes")]
     public static extern IntPtr GetNetworkBoxes(
         IntPtr networkPtr,
         int w, int h,
